0c9e424ba915581d9c46b56cbc552c3783690f91
alyconr <alyconr@hotmail.com>
2023-08-30 16:50:12 -0500

    :globe_with_meridians: update server express

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-const router = express.Router();
 
 // Middleware
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -13,12 +12,9 @@ app.use(bodyParser.json());
 // Serve static assets from the 'dist' directory
 app.use(express.static(path.join(__dirname, 'dist')));
 
-// Route all requests to the main HTML file
-router.get('*', (req, res) => {
+app.get ('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
-app.use(router);
+})
 
 // Start the server
 app.listen(PORT, () => {
